08cbbd594841869a25689ce366d821d1782b48a1
Aymeric DUJARDIN <aymeric.dujardin@gmail.com>
2021-04-08 12:37:27 +0200

    improve script

--- yolov5/run_timer.sh
+++ yolov5/run_timer.sh
@@ -10,16 +10,16 @@ yolov5_variants=("s" "m" "l" "x")
 image_sizes=("320" "384" "416" "512" "608" "640")
 
 # Compile for the first time
-mkdir build; cd build; cmake ..; make -j; cd ..
+mkdir build; cd build; cmake .. > /dev/null 2>&1; make -j > /dev/null 2>&1; cd ..
 
 for variant in "${yolov5_variants[@]}"; do
     # Get WTS file
-    wget "https://stereolabs.sfo2.cdn.digitaloceanspaces.com/ai/model/others/yolov5${variant}_v4.0.wts"
+    wget -nc -q "https://stereolabs.sfo2.cdn.digitaloceanspaces.com/ai/model/others/yolov5${variant}_v4.0.wts"
     for size in "${image_sizes[@]}"; do
         # Set size
         set_network_size ${size} ${size}
         # Compile
-        cd build; make; cd ..
+        cd build; make > /dev/null 2>&1 ; cd ..
         # Generate engine
         engine_name="./yolov5${variant}_${size}_fp16_v4.0.engine"
         build/yolov5 -s "./yolov5${variant}_v4.0.wts" ${engine_name} ${variant}
